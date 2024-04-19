932ce776ca4293bbd47b8071398f2b9b8fb51466
LeVperda <nikita.petrov573@gmail.com>
2024-04-19 14:28:19 +0300

    turn logic implemented

--- python/openCV_steering.py
+++ python/openCV_steering.py
@@ -1,8 +1,38 @@
 #middle point = 1/2 of horizontal resolution
-middle = 240
+import cv2
 
-r_line = 350
-l_line = 215
+img = cv2.imread(r"C:\Users\Monit\Desktop\Coding shit\robotics_project\test_photo\2024-04-19-122447.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+height, width, colorlayers = img.shape
+img.shape
+
+horizontal_line = int(height * (3/4))
+cv2.line(img, (0, horizontal_line), (width, horizontal_line), (255, 0, 0), 3)
+
+blue = 2
+green = 1
+red = 0
+
+l_line = 0
+
+for x in range(int(width/2), 0, -1):
+  if img[horizontal_line + 5, x][blue] >= 190 or img[horizontal_line + 5, x][green] >= 190 or img[horizontal_line + 5, x][red] >= 190:
+    l_line = x
+    break
+  continue
+
+
+r_line = 0
+
+for x in range(int(width/2), width):
+  if img[horizontal_line + 5, x][blue] >= 190 or img[horizontal_line + 5, x][green] >= 190 or img[horizontal_line + 5, x][red] >= 190:
+    r_line = x
+    break
+  continue
+  
+
+
+middle = int(width / 2)
 
 #compare the distance to each line from the middle point
 def get_turn():
